48a971cdc365d25c4a8a661dcd057dd755c1eac7
andrey <a.i.maslov199907@bk.ru>
2020-12-16 23:50:27 +0300

    добавлена автоматическая установка вебхука

--- src/flask_app.py
+++ src/flask_app.py
@@ -15,10 +15,10 @@ def getMessage():
 
 @server.route('/')
 def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://timetables-bot.herokuapp.com/" + TOKEN)
-    return "Hello from Heroku!", 200
+    return "Hello!", 200
 
 
 if __name__ == '__main__':
+    bot.remove_webhook()
+    bot.set_webhook('https://scheduling-everything.herokuapp.com/' + TOKEN)
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
