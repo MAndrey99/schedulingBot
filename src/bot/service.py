77651f9dd6bd292e9ac51aaa3f9ad4d9d057be03
andrey <a.i.maslov199907@bk.ru>
2021-01-10 21:13:12 +0300

    Предотвращение повторной инициализации логгера, указание Content-type при запросе к сервису, рефакторинг модуля bot.logs

--- src/bot/service.py
+++ src/bot/service.py
@@ -19,23 +19,27 @@ class ApiException(Exception):
 class Service:
     def __init__(self, url):
         self.url = url
+        self.json_headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
     def _request(self, method, path, method_from, **kwargs):
         response = method(self.url + path, **kwargs)
-        logs.info(f'{response.url} {response.status_code}')
+        logs.info(f'{method.__name__} {response.url} {response.status_code}')
         if response.status_code != 200:
             raise ApiException("Ошибка сервера: " + str(response.status_code), response.status_code, method_from)
         return json.loads(response.text) if response.text else None
 
     def get_deadlines(self, group_id: int, relevant=True) -> list[Deadline]:
         data = self._request(get, "deadlines", Service.get_deadlines,
-                                 params={'relevant': relevant, 'groupId': group_id})
+                             params={'relevant': relevant, 'groupId': group_id})
         if data["deadlines"] is None:
             return []
         return [Deadline.from_dict(it) for it in data["deadlines"]]
 
     def post_deadline(self, deadline: Deadline) -> Deadline:
-        return Deadline.from_dict(self._request(post, "deadlines", Service.post_deadline, data=deadline.to_json()))
+        return Deadline.from_dict(self._request(
+            post, "deadlines", Service.post_deadline,
+            data=deadline.to_json(), headers=self.json_headers
+        ))
 
     def delete_deadline(self, id: int, groupId: int):
         self._request(delete, f"deadlines/{id}", Service.delete_deadline, params={'groupId': groupId})
@@ -47,7 +51,10 @@ class Service:
         return [Deadline.from_dict(it) for it in data["deadlines"]]
 
     def patch_deadline(self, id: int, data: dict) -> Deadline:
-        return Deadline.from_dict(self._request(patch, f"deadlines/{id}", Service.patch_deadline, data=str(data)))
+        return Deadline.from_dict(self._request(
+            patch, f"deadlines/{id}", Service.patch_deadline,
+            data=str(data), headers=self.json_headers
+        ))
 
     def get_deadline(self, id: int) -> Deadline:
         return Deadline.from_dict(self._request(get, f"deadlines/{id}", Service.get_deadline))
